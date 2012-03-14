8bd9061ef4e04796b2e4a69da9eb6b44ea6a35a1
Joseph Lin <josephlin0922@gmail.com>
2012-03-14 15:10:14 -0400

    Initial Commit

---
+++ JLTableViewController/JLTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLTableViewController.h
+//  JLTableViewController
+//
+//  Created by Joseph Lin on 3/14/12.
+//  Copyright (c) 2012 R/GA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLTableViewController : NSObject
+
+@end
---
+++ JLTableViewController/JLTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  JLTableViewController.m
+//  JLTableViewController
+//
+//  Created by Joseph Lin on 3/14/12.
+//  Copyright (c) 2012 R/GA. All rights reserved.
+//
+
+#import "JLTableViewController.h"
+
+@implementation JLTableViewController
+
+@end
